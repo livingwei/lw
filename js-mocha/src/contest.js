ca015b0b3037f6f7e1d8999c2769d734b143c99e
livingwei <929887558@qq.com>
2019-03-18 17:06:21 +0800

    contestFirstCommit

--- js-mocha/src/contest.js
+++ js-mocha/src/contest.js
@@ -1,6 +1,142 @@
+
+var Item = [
+  {
+    shopName: 'Sulfuras',
+    SellIn: 'always',
+    Quality: 80
+  },
+  {
+    shopName: 'AgedBrie',
+    SellIn: 30,
+    Quality: 0
+  },
+  {
+    shopName: 'Backstagepasses',
+    SellIn: 'always',
+    Quality: 30
+  },
+  {
+    shopName: 'other',
+    SellIn: 30,
+    Quality: 30
+  }
+]
 class Contest {
-  method() {
-    return true;
+  totalWay (nameType, dateLine, cpType) {
+    for (var i = 0; i < Item.length; i++) {
+      if (Item[i].shopName === nameType) {
+        if (nameType === 'Sulfuras') {
+          return this.Sulfuras(Item[i], dateLine, cpType)
+        }
+        if (nameType === 'AgedBrie') {
+          return this.AgedBrie(Item[i], dateLine, cpType)
+        }
+        if (nameType === 'Backstagepasses') {
+          return this.Sulfuras(Item[i], dateLine, cpType)
+        }
+        if (nameType === 'other') {
+          return this.Sulfuras(Item[i], dateLine, cpType)
+        }
+      }
+    }
+  }
+  Sulfuras (ItemOne, dateLine, cpType) {
+    if (cpType === 'SellIn') {
+      return ItemOne.SellIn
+    } else if (cpType === 'Quality') {
+      return ItemOne.Quality
+    }
+  }
+  AgedBrie (ItemTwo, dateLine, cpType) {
+    if (dateLine > ItemTwo.SellIn) {
+      if (cpType === 'SellIn') {
+        return ItemTwo.SellIn
+      } else if (cpType === 'Quality') {
+        return ItemTwo.Quality
+      }
+    } else if (dateLine > 0) {
+      if (cpType === 'SellIn') {
+        return dateLine
+      } else if (cpType === 'Quality') {
+        if (ItemTwo.Quality * 2 - dateLine >= 50) {
+          return 50
+        } else {
+          return ItemTwo.Quality * 2 - dateLine
+        }
+      }
+    } else {
+      if (cpType === 'SellIn') {
+        return 0
+      } else if (cpType === 'Quality') {
+        if (ItemTwo.Quality * 2 + 2 * dateLine > 0) {
+          return ItemTwo.Quality * 2 + 2 * dateLine
+        } else {
+          return 0
+        }
+      }
+    }
+  }
+  BackstagePasses(ItemThree, dateLine, cpType) {
+    if (dateLine > 10) {
+      if (cpType === 'SellIn') {
+        return dateLine
+      } else if (cpType === 'Quality') {
+        if (ItemThree.Quality * 2 - dateLine >= 50) {
+          return 50
+        } else {
+          return ItemThree.Quality * 2 - dateLine
+        }
+      }
+    } else if (dateLine <= 10) {
+      if (cpType === 'SellIn') {
+        return dateLine
+      } else if (cpType === 'Quality') {
+        if (ItemThree.Quality * 2 - 10 + (10 - dateLine) * 2 >= 50) {
+          return 50
+        } else {
+          return ItemThree.Quality * 2 - 10 + (10 - dateLine) * 2
+        }
+      }
+    } else if (dateLine <= 5) {
+      if (cpType === 'SellIn') {
+        return dateLine
+      } else if (cpType === 'Quality') {
+        if (ItemThree.Quality * 2 - 10 + (10 - dateLine) * 2 - 5 + (5 - dateLine) * 3>= 50) {
+          return 50
+        } else {
+          return ItemThree.Quality * 2 - 10 + (10 - dateLine) * 2 - 5 + (5 - dateLine) * 3
+        }
+      }
+    } else {
+      if (cpType === 'SellIn') {
+        return 0
+      } else if (cpType === 'Quality') {
+        return 0
+      }
+    }
+  }
+  alwaysShop (ItemFour, dateLine, cpType) {
+    if (dateLine > 0) {
+      if (cpType === 'SellIn') {
+        return dateLine
+      } else if (cpType === 'Quality') {
+        if (ItemTwo.Quality * 2 - dateLine >= 50) {
+          return 50
+        } else {
+          return ItemTwo.Quality * 2 - dateLine
+        }
+      }
+    } else {
+      if (cpType === 'SellIn') {
+        return 0
+      } else if (cpType === 'Quality') {
+        if (ItemTwo.Quality * 2 + dateLine >= 50) {
+          return 50
+        } else {
+          return ItemTwo.Quality * 2 + dateLine
+        }
+      }
+    }
   }
 }
 
--- js-mocha/test/test_contest.js
+++ js-mocha/test/test_contest.js
@@ -2,9 +2,21 @@ var {expect} = require('chai');
 var {Contest} = require('../src/contest.js');
 describe("contest", function() {
 
-  it("shows a passed test", function() {
+  it("SulfurasFirstTest", function() {
     const contest = new Contest();
-    expect(contest.method()).to.equal(true);
+    expect(contest.totalWay('Sulfuras', 10, 'SellIn')).to.equal('always');
+  });
+  it("AgedBrieFirstTest", function() {
+    const contest = new Contest();
+    expect(contest.totalWay('AgedBrie', 10, 'SellIn')).to.equal(10);
+  });
+  it("BackstagepassesFirstTest", function() {
+    const contest = new Contest();
+    expect(contest.totalWay('Backstagepasses', 10, 'SellIn')).to.equal('always');
+  });
+  it("otherFirstTest", function() {
+    const contest = new Contest();
+    expect(contest.totalWay('other', 10, 'SellIn')).to.equal(30);
   });
 
 });
